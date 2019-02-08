a8f7b69dac28c6695e5e17fbb6f3f66b192329d2
Blahak Jeremy <jeremy.blahak@polytechnique.edu>
2019-02-08 21:43:49 +0100

    ajout élement front manquant

--- src/resources/js/index.js
+++ src/resources/js/index.js
@@ -1,5 +1,33 @@
 $(document).ready(function(){
   var mySwiper = new Swiper ('.swiper-container', {
-    autoHeight: true
+    autoHeight: true,
+  });
+
+  $("#geocoder").click(function(){
+    $(".reglage__section").css({"transform": "translateY(0)", "opacity": "1", "z-index": "4"});
+  });
+
+  function revealModal(arg){
+    var id = arg.attr("id");
+    $('.'+id+'-modal').css({"display": "flex"});
+    $('.'+id+'-modal').animate({"opacity": "1"}, 300);
+    $('.modal').animate({"opacity": "1"}, 300);
+    $('body').css({"overflow-y": "hidden"});
+  }
+
+  var arr = ['ajout-vehicle__card', 'name__modif', 'mdp__modif', 'mail__modif'];
+  $('.open-modal').click(function(){
+    for (var i = 0; i < arr.length; i++){
+      if ($(this).hasClass(arr[i])){
+        revealModal($(this));
+      }
+    }
+  });
+
+  $('.close-modal').click(function(){
+    $('.container-modal').fadeTo(300, 0, function(){
+      $('.container-modal').css({"display": "none"});
+      $('body').css({"overflow-y": "auto"});
+    });
   });
 });
